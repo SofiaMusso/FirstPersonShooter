de4ee4985b01da722c5b97dbb475141664e4f2d3
Miriana <spalmalupi@gmail.com>
2026-01-28 11:11:49 +0100

    movimento del player

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    CharacterController cc;
+
+    public float speed = 12f;
+    public float gravity = -9.8f;
+    public float jumpHeight = 1.5f;
+
+    bool isGrounded;
+
+    Vector3 velocity;
+
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = cc.isGrounded;
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        cc.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -3.0f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        cc.Move(velocity * Time.deltaTime);
+    }
+}
